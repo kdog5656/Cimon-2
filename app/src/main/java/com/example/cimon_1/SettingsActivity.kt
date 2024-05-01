f872fa45a222a510b4cce3996b7307d6ceac2489
Bannanamae <125344782+Bannanamae@users.noreply.github.com>
2024-05-01 16:10:27 -0500

    UI Update

--- app/src/main/java/com/example/cimon_1/SettingsActivity.kt
+++ app/src/main/java/com/example/cimon_1/SettingsActivity.kt
@@ -5,11 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 
-class SettingsActivity : Activity()  {
+class SettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
     }
+
     fun onClick(view: View) {
 
         when (view.id) {
@@ -19,35 +20,11 @@ class SettingsActivity : Activity()  {
                 val audioButtonIntent = Intent(this, GamePageActivity::class.java)
                 startActivity(audioButtonIntent)
             }
-            R.id.None_Button -> {
 
-                val noneButtonIntent = Intent(this, GamePageActivity::class.java)
-                startActivity(noneButtonIntent)
-            }
-            R.id.Trianopia_Button -> {
-
-                val trianopiaButtonIntent = Intent(this, GamePageActivity::class.java)
-                startActivity(trianopiaButtonIntent)
-            }
-            R.id.Deuteranopia_Button -> {
-
-                val deuteranopiaButtonIntent = Intent(this, GamePageActivity::class.java)
-                startActivity(deuteranopiaButtonIntent)
-            }
-            R.id.Protanopia_Button -> {
-
-                val protanopiaButtonIntent = Intent(this, GamePageActivity::class.java)
-                startActivity(protanopiaButtonIntent)
-            }
             R.id.Sounds_Button -> {
 
                 val soundsButtonIntent = Intent(this, GamePageActivity::class.java)
                 startActivity(soundsButtonIntent)
-            }
-            R.id.Darkmode_Button -> {
-
-                val darkmodeButtonIntent = Intent(this, GamePageActivity::class.java)
-                startActivity(darkmodeButtonIntent)
             }
         }
     }
