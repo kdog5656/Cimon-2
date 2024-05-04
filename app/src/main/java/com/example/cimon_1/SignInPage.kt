e83449545939862f001aba5aa8707843fdbcea06
kris <khorchover@gmail.com>
2024-05-04 16:23:18 -0400

    adding login/signup/signin pages

---
+++ app/src/main/java/com/example/cimon_1/SignInPage.kt
@@ -0,0 +1,33 @@
+package com.example.cimon_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInPage : AppCompatActivity() {
+
+    private lateinit var btnInsertData: Button
+    private lateinit var btnFetchData: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnInsertData = findViewById(R.id.btnInsertData)
+        btnFetchData = findViewById(R.id.btnFetchData)
+
+        btnInsertData.setOnClickListener {
+            val intent = Intent(this, InsertionActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnFetchData.setOnClickListener {
+            val intent = Intent(this, FetchingActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
